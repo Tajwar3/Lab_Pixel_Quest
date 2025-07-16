3a9fef7595c6b526c6a773553161fcb4a51d3b66
Tajwar3 <tajwarjahin92@gmail.com>
2025-07-16 15:51:43 -0400

    bankai

--- Assets/Geo_Quest/Scripts/NewBehaviourScript.cs
+++ Assets/Geo_Quest/Scripts/NewBehaviourScript.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -77,14 +76,6 @@ public class NewBehaviourScript : MonoBehaviour
 
             }
     }
-
-
-
-
-
-
-
-
 
 
 
