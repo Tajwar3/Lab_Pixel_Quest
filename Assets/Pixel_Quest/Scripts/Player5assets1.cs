08a7a61aba7fd35fcb26dd6f36441a0d41858852
Tajwar3 <tajwarjahin92@gmail.com>
2025-07-23 12:21:18 -0400

    idk1

--- Assets/Pixel_Quest/Scripts/Player5assets1.cs
+++ Assets/Pixel_Quest/Scripts/Player5assets1.cs
@@ -8,18 +8,28 @@ public class Player5assets1 : MonoBehaviour
 {
     public string nextLevel = "nextLevel";
     public int Coincount = 0;
-    public int HealthCount = 0;
+    public int HealthCount = 3;
+    public Transform respawnPoint;
 
     private void OnTriggerEnter2D(Collider2D other)
+
     {
 
         switch (other.tag)
-        {
+        { 
             case "Death":
                 {
-                    string thisLevel = SceneManager.GetActiveScene().name;
-                    SceneManager.LoadScene(thisLevel);
-                    break;
+                    HealthCount--;
+                if (HealthCount <= 0)
+                {string thisLevel = SceneManager.GetActiveScene().name;
+                        SceneManager.LoadScene(thisLevel);
+                 }
+                    else
+                    { transform.position = respawnPoint.position; }
+                        break;
+
+                
+
                 }
             case "Finish":
                 {
@@ -34,11 +44,26 @@ public class Player5assets1 : MonoBehaviour
                 }
             case "Health":
                 {
+                    if (HealthCount <= 3)
+                    {
+                        HealthCount++;
+                        Destroy(other.gameObject);
+                        break;
+                    }
+                       
                     HealthCount++;
                     Destroy(other.gameObject);
                     break;
                 }
 
+            case "Respawn":
+                respawnPoint.position = other.transform.Find("Point").position;
+                break;
+            case "Finish":
+                {
+                    string nextLevel = other.GetComponent<LevelGoal>();
+                    break;
+                }
 
 
 
